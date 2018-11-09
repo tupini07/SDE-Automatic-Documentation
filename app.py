b038b02fcd2e965f115f1208634cb32b89c33366
Andrea Tupini <tupini07@gmail.com>
2018-11-09 23:52:21 +0100

    converted app to todo manager

--- app.py
+++ app.py
@@ -4,25 +4,38 @@ from flask import Flask, redirect, url_for, render_template, request, session
 import json
 import sys
 import os
-
+import random
 
 app = Flask(__name__)
 
 
-@app.route("/post/<int:post_id>", methods=["GET"])
-def get_specific_post(post_id):
-    """Return information on a specific post.
+todo_db = {  # todo  map, id:text
+    1429: "Buy milk",
+    3821: "Walk my dog",
+    2952: "Do some exercise"
+}
 
-    .. :quickref: Get specific post; Gets the information about a specific potst.
+
+@app.after_request
+def apply_caching(response):
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.route("/todo/<int:todo_id>", methods=["GET"])
+def get_specific_todo(todo_id):
+    """Return information on a specific ToDo item.
+
+    .. :quickref: Get specific ToDo; Gets the information about a specific ToDo.
 
     **Example request:**
 
     .. sourcecode:: http
 
-        GET /post/post_id HTTP/1.1
+        GET /todo/4278 HTTP/1.1
         Host: example.com
         Accept: application/json
-    
+
 
     **Example response:**
 
@@ -33,38 +46,33 @@ def get_specific_post(post_id):
         Content-Type: application/json
 
         {
-            "post_id": 123123,
-            "author": "/author/123/",
-            "tags": ["python3", "typehints", "annotations"],
-            "title": "To typehint or not to typehint that is the question",
-            "body": "Static checking in python..."
+            "id": 4278,
+            "text": "Work on SDE project"
         }
 
-    :query post_id: The id of the post we want to get specific information of
-    :status 200: post found
-    :status 404: post with given id not found
+    :query post_id: The id of the todo we want to get specific information about
+    :status 200: todo found
+    :status 404: todo with given id not found
     """
-    return json.dumps({
-        "post_id": post_id,
-        "author": "/author/123/",
-        "tags": ["python3", "typehints", "annotations"],
-        "title": "To typehint or not to typehint that is the question",
-        "body": "Static checking in python..."
-    })
+
+    if todo_id in todo_db.keys():
+        return json.dumps({"id": todo_id, "text": todo_db[todo_id]}), 200
+
+    else:
+        return json.dumps({"error": "ToDo not found."}), 404
 
 
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    """Return all todos.
 
-@app.route('/posts', methods=['GET'])
-def get_posts():
-    """Return collection of posts.
-
-    .. :quickref: Posts Collection; Get collection of posts.
+    .. :quickref: ToDo collection; Get all ToDos.
 
     **Example request**:
 
     .. sourcecode:: http
 
-        GET /posts/ HTTP/1.1
+        GET /todos HTTP/1.1
         Host: example.com
         Accept: application/json
 
@@ -77,82 +85,82 @@ def get_posts():
         Content-Type: application/json
 
         [
-        {
-            "post_id": 12345,
-            "author": "/author/123/",
-            "tags": ["sphinx", "rst", "flask"],
-            "title": "Documenting API in Sphinx with httpdomain",
-            "body": "How to..."
-        },
-        {
-            "post_id": 12346,
-            "author": "/author/123/",
-            "tags": ["python3", "typehints", "annotations"],
-            "title": "To typehint or not to typehint that is the question",
-            "body": "Static checking in python..."
-        }
+            {
+                "id": 2231,
+                "text": "Take a shower",
+            },
+            {
+                "id": 2243,
+                "text": "Bake some cookies",
+            },
         ]
 
-    :query sort: sorting order e.g. sort=author,-pub_date
-    :query q: full text search query
-    :resheader Content-Type: application/json
+    :query sort: direction of sort, either `asc` or `desc`. Elements are sorted by `id`. Default is `asc`
     :status 200: posts found
+    :status 204: Returned when there are no todos 
+    :status 400: If sort parameter is not valid
     """
-    return json.dumps([
-        {
-            "post_id": 12346,
-            "author": "/author/123/",
-            "tags": ["python3", "typehints", "annotations"],
-            "title": "To typehint or not to typehint that is the question",
-            "body": "Static checking in python..."
-        }
-    ])
+
+    sortdir = request.args.get("sort", "asc")
+
+    if sortdir not in ["desc", "asc"]:
+        return json.dumps({"error": "Sort parameter is invalid, must be either `asc` or `desc`"}), 400
+
+    ids = sorted(todo_db.keys(), reverse=(sortdir == "desc"))
+
+    return json.dumps(list({"id": id, "text": todo_db[id]} for id in ids))
 
 
-@app.route('/posts', methods=['POST'])
-def create_post():
-    """Create a post.
+@app.route('/todo', methods=['POST'])
+def create_todo():
 
-    .. :quickref: Create a post; Creates a post with the specified content.
-
-    **Example request**:
-
-    .. sourcecode:: http
-
-        POST /posts/ HTTP/1.1
-        Host: example.com
-        Accept: application/json
-
-        {
-            "content": "Some content for the post",
-            "author_id": 39,
-            "publishing_date": "30/9/2019"
-        }
-
-    **Example response**:
-
-    .. sourcecode:: http
-
-        HTTP/1.1 200 OK
-        Vary: Accept
-        Content-Type: application/json
-
-        {
-            "post_id": 39203,
-        }
+    if "text" not in request.form.keys():
+        return json.dumps({"error": "No text provided for the new todo!"}), 400
+    
+    else:
+        new_id = random.randint(1000, 9999) # note: no check for id clashes
+        todo_db[new_id] = request.form.get("text")
+        return json.dumps({"id": new_id}), 201 # successfully created
 
 
-    :query content: Textual content
-    :query author_id: ID of the author that created the post.
-    :query publishing_date: The date we want the post to be published.
+@app.route("/todo/<int:todo_id>", methods=["PUT"])
+def update_specific_todo(todo_id):
 
-    :resheader Content-Type: application/json
-    :status 201: post created
-    """
-    return json.dumps({"post_id": 23213})
+    if todo_id not in todo_db.keys():
+        return json.dumps({"error": "ToDo not found."}), 404
+        
+    elif "text" not in request.form.keys():
+        return json.dumps({"error": "No text was provided to update the TodDo!"}), 400
+
+    else:
+        todo_db[todo_id] = request.form.get("text")
+        return get_specific_todo(todo_id)[0], 202
+
+
+
+@app.route("/todo/<int:todo_id>", methods=["DELETE"])
+def delete_specific_todo(todo_id):
+
+    if todo_id not in todo_db.keys():
+        return json.dumps({"error": "ToDo not found."}), 404
+        
+    else:
+        todo_db.pop(todo_id)
+        return get_todos(), 202
+
+
+@app.route("/todos", methods=["DELETE"])
+def delete_all_todos():
+
+    todo_db.clear()
+    return "[]", 202
+
+
 
 
 # ======== Main ============================================================== #
 if __name__ == "__main__":
+
+    os.environ["FLASK_ENV"] = "development"
     app.secret_key = os.urandom(12)  # Generic key for dev purposes only
     app.run(debug=True, use_reloader=True)
